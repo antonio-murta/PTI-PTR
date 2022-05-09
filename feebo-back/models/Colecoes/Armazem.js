659f972dea294ecd4e961df3b98e1a2801077602
LampreiaRGB <fc54957@alunos.fc.ul.pt>
2022-05-09 15:31:14 +0100

    Alteracao das funcoes v2 (backup)

--- feebo-back/models/Colecoes/Armazem.js
+++ feebo-back/models/Colecoes/Armazem.js
@@ -1,100 +1,28 @@
 const monggose = require('mongoose');
-const ArmazemModel = require("../Colecoes/Armazem");
-// const conexao = require('../../conexao');
-const bcrypt = require('bcryptjs');
 
+const ArmazemSchema = new monggose.Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    morada: {
+        type: String,
+        required: true,
+    },
+    telemovel: {
+        type: Number,
+        required: true,
+    },
+    produtos: {
+        type: [],
+        required: true,
+    }
+});
 
+//fornecedorCollection.update_one(
+//    {"_id": emailFornecedor},
+//    {"$push": {"armazens": idArmazem} }
+//)
 
-
-const criarArmazem = (req, res) => {
-    const armazem = new ArmazemModel(req.body);
-    armazem.save()
-    .then(() => {
-        res.status(201).send("Armazem criado com sucesso");
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-const getByNome = (req, res) => {
-    const nome = req.body.nome;
-
-    ArmazemModel.find({nome: nome})
-    .then(result => {
-        if (result.length == 0) {
-            res.status(404).send("nao encontrado");
-        } else {
-            res.status(200).send(result);
-        }
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-const getByMorada = (req, res) => {
-    const morada = req.body.morada;
-
-    ArmazemModel.find({morada: morada})
-    .then(() => {
-        if (result.length == 0) {
-            res.status(404).send("nao encontrado");
-        } else {
-            res.status(200).send(result);
-        }
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-const getByTelemovel = (req, res) => {
-    const telemovel = req.body.telemovel;
-
-    ArmazemModel.find({telemovel: telemovel})
-    .then(() => {
-        if (result.length == 0) {
-            res.status(404).send("nao encontrado");
-        } else {
-            res.status(200).send(result);
-        }
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-const deleteById = (req, res) => {
-    const id = req.params.id;
-
-    ArmazemModel.deleteOne({_id: id})
-    .then(count => {
-        if (count == 0) {
-            res.status(404).send("nao encontrado");
-        } else {
-            res.status(200).send("Armazem apagado com sucesso");
-        }
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-const deleteByNome = (req, res) => {
-    const nome = req.params.nome;
-
-    ArmazemModel.deleteMany({nome: nome})
-    .then(count => {
-        if (count == 0) {
-            res.status(404).send("nao encontrado");
-        } else {
-            res.status(200).send("Armazem apagado com sucesso");
-        }
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-}
-
-module.exports = {criarArmazem, getByMorada, getByNome, getByTelemovel, deleteByNome, deleteById};
+const armazem = monggose.model("armazen", ArmazemSchema);
+module.exports = armazem;
--- feebo-back/models/Funcoes/F_Utilizador.js
+++ feebo-back/models/Funcoes/F_Utilizador.js
@@ -3,127 +3,134 @@ const UtilizadorModel = require("../Colecoes/Utilizador");
 const ConsumidorModel = require("../Colecoes/Consumidor");
 const FornecedorModel = require("../Colecoes/Fornecedor");
 const TransportadorModel = require("../Colecoes/Transportador");
-const conexao = require('../../conexao');
+//const conexao = require('../../conexao');
 const bcrypt = require('bcryptjs');
 
 
 
 
-
-
-
-
-
-
-
-// var bcrypt = require('bcryptjs');
-// var salt = bcrypt.genSaltSync(10);
-// var hash = bcrypt.hashSync("B4c0/\/", salt);
-
-
-
-
-async function registar (email, nome, dataNasc, nif, morada, telemovel, password, utipo)
-{
-    await conexao;
-    var salt = await bcrypt.genSaltSync(10);
+const registar = (req, res) => {
+    let utipo = req.body.utipo;
+    // var salt = bcrypt.genSaltSync(10);
     //console.log(salt)
-    var passwordEnc = await bcrypt.hashSync(password, salt);
-    // encryptedPassword = await bcrypt.hash(password, 10);
-    await UtilizadorModel.create (
+    // req.body.password = String( bcrypt.hashSync(req.body.password, salt));
+    const utilizador = new UtilizadorModel(req.body);
+    utilizador.save()
+    .then(() => {
+        if (utipo == "consumidor") {
+            ConsumidorModel.create(
+                {
+                    _id: email,
+                    metodoPagamento: [],
+                    cesto: [],
+                    recursosCesto: [],
+                    poluicaoCesto: 0
+                }
+            )
+            .then(() => {
+                res.status(201).send("Utilizador criado com sucesso");
+            });
+        }
+        else if (utipo == "fornecedor") {
+            FornecedorModel.create(
+                {
+                    _id: email,
+                    armazens: [],
+                    produtos: []
+                }
+            )
+            .then(() => {
+                res.status(201).send("Utilizador criado com sucesso");
+            });
+        }
+        else {
+            TransportadorModel.create(
+                {
+                    _id: email,
+                    caminhos: [],
+                    veiculo: null
+                }
+            )
+            .then(() => {
+                res.status(201).send("Utilizador criado com sucesso");
+            });
+        }
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+}
+
+const login = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let utilizador = UtilizadorModel.findOne({_id: email});
+    let result = bcrypt.compare(password, utilizador["password"]).valueOf();
+    if (result) {
+        res.status(200).send("login feito com sucesso");
+    } else {
+        res.status(400).send("email/password invalidos");
+    }
+}
+
+const editarConta = (req, res) => {
+    const email = req.params.id;
+    UtilizadorModel.updateOne(
+        { _id: email },
         {
-            _id: email,
-            nome: nome,
-            dataNasc: dataNasc,
-            nif: nif,
-            morada: morada,
-            telemovel: telemovel,
-            password: passwordEnc,
-            utipo: utipo
+            morada: req.body.morada,
+            telemovel: req.body.telemovel
         }
     )
-
-    if (utipo == "consumidor")
-    {
-        await ConsumidorModel.create (
-            {
-                _id: email,
-                metodoPagamento: [],
-                cesto: [],
-                recursosCesto: [],
-                poluicaoCesto: 0
-            }
-        )
-    }
-    else if (utipo == "fornecedor")
-    {
-        await FornecedorModel.create (
-            {
-                _id: email,
-                armazens: [],
-                produtos: []
-            }
-        )
-    }
-    else
-    {
-        await TransportadorModel.create (
-            {
-                _id: email, 
-                caminhos: [],
-                veiculo: null
-            }
-        )
-    }
-
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
 }
 
-//async function registar (email, nome, dataNasc, nif, morada, telemovel, password, utipo)
-registar ("teota@gmail.com", "teota", "12.12.1212", 12345, "Rua26", 765489321, "123", "transportador")
-
-async function login (email, password)
-{
-    await conexao;
-    const utilizador = await UtilizadorModel.findOne({_id: email});
-    console.log(await bcrypt.compare(password, utilizador["password"]));
-
-}
-
-
-async function editarConta (email, morada, telemovel)
-{
-    await conexao;
-    await UtilizadorModel.updateOne(
-        {_id: email},
-        {
-            morada: morada,
-            telemovel: telemovel
-        }
-    )
-}
-// editarConta("catarina@gmail.com", "Rua", 987654321);
-
-
-async function alterarPassword (email, passwordAntiga, passwordNova)
-{
-    await conexao;
-    const utilizador = await UtilizadorModel.findOne({_id: email});
-    if (await bcrypt.compare(passwordAntiga, utilizador["password"]))
-    {
-        const salt = await bcrypt.genSalt(10);
-        passwordNovaEnc = await bcrypt.hash(passwordNova, salt);
-        await UtilizadorModel.updateOne(
-            {_id: email},
+const alterarPassword = (req, res) => {
+    const email = req.params.id;
+    const utilizador = UtilizadorModel.findOne({ _id: email });
+    if (bcrypt.compare(req.body.passwordAntiga, utilizador["password"])) {
+        const salt = bcrypt.genSalt(10);
+        passwordNovaEnc = bcrypt.hash(req.body.passwordNova, salt);
+        UtilizadorModel.updateOne(
+            { _id: email },
             {
                 password: passwordNovaEnc
             }
         )
-        return true
+        .then(() => {
+            res.status(200).send("Password alterada com sucesso");
+        })
     }
-    else
-    {
-        return false
+    else {
+        res.status(400).send("Password antiga nao corresponde com a dada");
     }
 }
 
-module.exports = {registar, editarConta, editarConta, alterarPassword, login}
+const apagarUtilizadores = (req, res) => {
+    UtilizadorModel.deleteMany({})
+    .then(result => {
+        res.status(200).send("Utilizadores apagados com sucesso");
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    })
+}
+
+
+const apagarUtilizadores_byID = (req, res) => {
+    UtilizadorModel.findByIdAndDelete(req.body.email)
+    .then(() => {
+        res.status(200).send("Utilizador apagado com sucesso");
+    })
+    .catch(() => {
+        res.status(404).send("Utilizador nao existe");
+    })
+}
+
+module.exports = { registar, editarConta, alterarPassword, login, apagarUtilizadores, apagarUtilizadores_byID }
