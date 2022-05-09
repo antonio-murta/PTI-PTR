ac67b56ac3bf8325e8783e48ecff365cda0add54
Izma05 <catatravanca@gmail.com>
2022-05-09 15:22:18 +0100

    alteração das funcoes v1

--- feebo-back/models/Funcoes/F_Tranporte.js
+++ feebo-back/models/Funcoes/F_Tranporte.js
@@ -4,43 +4,83 @@ const conexao = require('../../conexao');
 // const bcrypt = require('bcrypt');
 
 
-async function criarTransportador(email, caminhos, veiculo){
-    await conexao;
 
-    await TransporteModel.create(
+
+const criarTransportador = (req, res) => {
+    const transportador = new TransporteModel(req.body);
+    transportador.save()
+    .then(() => {
+        res.status(201).send("Transportador criado com sucesso");
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+}
+
+const getByNome = (req, res) => {
+    const nome = req.body.nome;
+
+    TransporteModel.find({nome: nome})
+    .then(result => {
+        if (result.length == 0) {
+            res.status(404).send("nao encontrado");
+        } else {
+            res.status(200).send(result);
+        }
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+}
+
+
+
+
+const updateCaminhos = (req, res) => {
+    const email = req.params.id;
+    TransporteModel.updateOne(
+        { _id: email },
         {
-            _id: email,
-            caminhos: caminhos,
-            veiculo: veiculo
+            caminhos: req.body.caminhos
         }
     )
-}
-// criarTransportador("Duartee@gmail.com", [], "AS-12-AS")
-
-
-async function getByNome(email){
-    await conexao;
-
-    const transportador = await TransporteModel.findOne({_id: email})
-    console.log(transportador)
-    return transportador
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
 }
 
-async function updateCaminhos(email, caminhos){
-    await conexao;
 
-    await TransporteModel.updateOne({_id:email},{caminhos: caminhos})
+
+
+const updateVeiculo = (req, res) => {
+    const email = req.params.id;
+    TransporteModel.updateOne(
+        { _id: email },
+        {
+            veiculo: req.body.veiculo
+        }
+    )
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
 }
 
-async function updateVeiculo(email, veiculo){
-    await conexao;
 
-    await TransporteModel.updateOne({_id:email},{veiculo: veiculo})
+const deleteByEmail = (req, res) => {
+    TransporteModel.findByIdAndDelete(req.body.email)
+    .then(() => {
+        res.status(200).send("Utilizador apagado com sucesso");
+    })
+    .catch(() => {
+        res.status(404).send("Utilizador nao existe");
+    })
 }
 
-async function deleteByEmail(email){
-    await conexao;
 
-    await TransporteModel.deleteOne({email: email})
-}
-
+module.exports = {/*getByNome, */updateCaminhos, updateVeiculo, deleteByEmail};
