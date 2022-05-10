e42085bfa3ef64ec35bcc8943c513f5d937ce875
antonio-murta <antoniomurteir@gmail.com>
2022-05-10 12:57:51 +0100

    organização armazém

--- feebo-front/my-app/src/pages/fornecedor/armazem/infoArmazem.jsx
+++ feebo-front/my-app/src/pages/fornecedor/armazem/infoArmazem.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Notif from "./notificacoes"
-import Armazens from "./todosArmazens"
-import "./style.css"
+import Notif from "../mainpage/notificacoes"
+import Armazens from "../mainpage/produtosArmazem"
+import "../mainpage/style.css"
 
 const Main = ({ addToCart, shopItems }) => {
   return (
@@ -13,9 +13,12 @@ const Main = ({ addToCart, shopItems }) => {
           <div className='contentWidth'>
             <div className='heading d_flex'>
               <div className='heading-left row  f_flex'>
-                <h2>Armazéns</h2>
+                <h2>Nome Armazém</h2>
               </div>
-             
+              <div className='heading-right row '>
+                <span>Ver todos</span>
+                <i className='fa-solid fa-caret-right'></i>
+              </div>
             </div>
             <div className='product-content  grid1'>
               <Armazens addToCart={addToCart} shopItems={shopItems} />
