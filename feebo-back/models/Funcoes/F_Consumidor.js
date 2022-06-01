83043950a14861bf21fbc5a99e21ab8a94efb7cb
Izma05 <catatravanca@gmail.com>
2022-06-01 17:46:37 +0100

    coisa

--- feebo-back/models/Funcoes/F_Consumidor.js
+++ feebo-back/models/Funcoes/F_Consumidor.js
@@ -28,27 +28,73 @@ async function visualizarPoluicaoCesto (email)
 
 
 
-async function encomendarCesto (email, emailTransportador, pagamento)
-{
-    now = new Date;
-    await conexao;
-    const Utilizador = await ConsumidorModel.findOne({_id: email});
-    const produtos = Utilizador["cesto"];
+// async function encomendarCesto (email, emailTransportador, pagamento)
+// {
+//     now = new Date;
+//     await conexao;
+//     const Utilizador = await ConsumidorModel.findOne({_id: email});
+//     const produtos = Utilizador["cesto"];
 
-    // let data = moment().format();
+//     // let data = moment().format();
 
 
-    const data = now.getDate() + "." + now.getMonth() + "." + now.getFullYear()
+//     const data = now.getDate() + "." + now.getMonth() + "." + now.getFullYear()
 
-    await EncomendaModel.create (
-        {
-            cliente:  email,
-            data_inicio:  data,
-            produtos: produtos,
-            transporte: emailTransportador,
-            pagamento:  pagamento
+//     await EncomendaModel.create (
+//         {
+//             cliente:  email,
+//             data_inicio:  data,
+//             produtos: produtos,
+//             transporte: emailTransportador,
+//             pagamento:  pagamento
+//         }
+//     )
+// }
+
+const get_all = (req, res) => {
+    ConsumidorModel.find({})
+    .then(result => {
+        if (result.length == 0) {
+            res.status(404).send("Nao existem consumidores registados");
+        } else {
+            res.status(200).send(result);
         }
-    )
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    })
 }
+
+
+
+const encomendarCesto = (req, res) => {
+    // const email = req.params.id;
+    const email = "catarina@gmail.com";
+
+    ConsumidorModel.findOne({_id: email})
+    .then(result => {
+        if (result.length == 0) {
+            res.status(404).send("nao encontrado");
+        } else {
+
+            const cesto = result[0].cesto;
+            res.status(200).send(cesto);
+        
+
+
+            res.status(200).send(result);
+        }
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+
+
+}
+
+
+module.exports = { get_all, encomendarCesto }
+
+
 // pagamento = ["cartao", 123, "carlota", "02/23", 143]
 // encomendarCesto ("catarina@gmail.com", "Duarte@gmail.com", pagamento)
