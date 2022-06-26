99b550549878ac9c8ff553255b25c00d76afe229
antonio-murta <antoniomurteir@gmail.com>
2022-06-26 23:09:45 +0100

    armazens aparece bd
    aramzens aparece bd

--- feebo-front/my-app/src/pages/fornecedor/fornecedor.js
+++ feebo-front/my-app/src/pages/fornecedor/fornecedor.js
@@ -1,14 +1,28 @@
-import React from "react"
-import Main from "../../pages/fornecedor/mainpage/mainpage"
-import Wrapper from "../../components/wrapper/Wrapper"
+import React from 'react';
+import Main from '../../pages/fornecedor/mainpage/mainpage';
+import Wrapper from '../../components/wrapper/Wrapper';
 
-const Fornecedor = ({productItems, addToCart, CartItem, infoArmazens }) => {
+const Fornecedor = ({
+  productItems,
+  addToCart,
+  CartItem,
+  infoArmazens,
+  armazens,
+  setArmazens,
+  todosarmazens,
+}) => {
   return (
     <>
-      <Main infoArmazens={infoArmazens} addToCart={addToCart} />
+      <Main
+        infoArmazens={infoArmazens}
+        addToCart={addToCart}
+        armazens={armazens}
+        setArmazens={setArmazens}
+        todosarmazens={todosarmazens}
+      />
       <Wrapper />
     </>
-  )
-}
+  );
+};
 
-export default Fornecedor
+export default Fornecedor;
