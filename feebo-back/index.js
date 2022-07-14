1515d99b72b660176c4722380d8f5c49220be0ec
bestadoce <73438336+bestadoce@users.noreply.github.com>
2022-07-15 00:53:06 +0100

    nada de mais

--- feebo-back/index.js
+++ feebo-back/index.js
@@ -30,14 +30,11 @@ app.delete("/produto/:id", FornecedorController.removerProduto);
 
 app.get("/fornecedores/:id", FornecedorController.getById);
 
-
-
 app.put("/updateArmazem/:id", ArmazemController.updateArmazem);
 app.get(
   "/armazens/fornecedor/:id",
   ArmazemController.get_all_armazens_Fornecedor
 );
-
 
 // Produto
 app.get("/produto", ProdutoController.get_all);
@@ -53,32 +50,33 @@ app.delete("/produto", FornecedorController.removerProdutos);
 app.delete("/produto/:id", FornecedorController.removerProduto);
 
 // Transportador
-app.post('/utilizador/veiculo', TransportadorControler.criarVeiculo);
-app.get('/veiculos', TransportadorControler.get_all_veiculos);
-app.delete('/veiculo/:id', TransportadorControler.deleteVeiculoById);
-app.put('/utilizador/:id/veiculo', TransportadorControler.updateVeiculo);
-app.get('/veiculos/transportador/:id', TransportadorControler.get_all_veiculos_Transportador);
-app.get('/veiculo/:id', TransportadorControler.getById_veiculos);
-
+app.post("/utilizador/veiculo", TransportadorControler.criarVeiculo);
+app.get("/veiculos", TransportadorControler.get_all_veiculos);
+app.delete("/veiculo/:id", TransportadorControler.deleteVeiculoById);
+app.put("/utilizador/:id/veiculo", TransportadorControler.updateVeiculo);
+app.get(
+  "/veiculos/transportador/:id",
+  TransportadorControler.get_all_veiculos_Transportador
+);
+app.get("/veiculo/:id", TransportadorControler.getById_veiculos);
 
 // Armazem
-app.post('/armazem', ArmazemController.criarArmazem);
-app.get('/armazem/nome', ArmazemController.getByNome);
-app.get('/armazem', ArmazemController.get_all);
-app.get('/armazem/:id', ArmazemController.getById);
-app.get('/armazem/morada', ArmazemController.getByMorada);
-app.get('/armazem/telemovel', ArmazemController.getByTelemovel);
-app.delete('/armazem', ArmazemController.deleteByNome);
-app.delete('/armazem/:id', ArmazemController.deleteById);
-
+app.post("/armazem", ArmazemController.criarArmazem);
+app.get("/armazem/nome", ArmazemController.getByNome);
+app.get("/armazem", ArmazemController.get_all);
+app.get("/armazem/:id", ArmazemController.getById);
+app.get("/armazem/morada", ArmazemController.getByMorada);
+app.get("/armazem/telemovel", ArmazemController.getByTelemovel);
+app.delete("/armazem", ArmazemController.deleteByNome);
+app.delete("/armazem/:id", ArmazemController.deleteById);
 
 // Consumidor
 app.get("/consumidor", ConsumidorController.get_all);
 app.post("/encomenda", ConsumidorController.encomendarCesto);
 
-app.get("/encomendas", ConsumidorController.get_all_Encomendas);
-app.get("/encomenda/:id", ConsumidorController.getById_Encomendas);
-app.delete("/encomenda/:id", ConsumidorController.deleteEncomendaById);
+app.get("/encomendas", ConsumidorController.get_all_Encomendas); //new
+app.get("/encomenda/:id", ConsumidorController.getById_Encomendas); //new
+app.delete("/encomenda/:id", ConsumidorController.deleteEncomendaById); //new
 
 app.get("/encomendas", ConsumidorController.get_all_Encomendas);
 app.get("/encomenda/:id", ConsumidorController.getById_Encomendas);
